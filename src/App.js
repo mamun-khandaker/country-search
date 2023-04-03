bf375a5d638f7d7ac0071d73484ba35bbdb9f441
Mamun Khandaker <mamun-fe@Mamuns-MacBook-Pro.local>
2023-04-03 21:36:29 +0600

    Full project upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Countries from "./components/Countries";
+import Search from "./components/Search";
+
+const url = "https://restcountries.com/v3.1/all";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState(countries);
+
+  const fetchCountries = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setFilteredCountries(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCountries(url);
+  }, []);
+
+  const countryRemove = (name) => {
+    const removedCountries = filteredCountries.filter(
+      (country) => country.name.common !== name
+    );
+    setFilteredCountries(removedCountries);
+  };
+
+  const handleSearch = (searchVal) => {
+    const lowerVal = searchVal.toLowerCase();
+    const newCountryName = countries.filter((country) => {
+      const name = country.name.common.toLowerCase();
+
+      return name.startsWith(lowerVal);
+    });
+    setFilteredCountries(newCountryName);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Country details
+        <Search onSearch={handleSearch} />
+      </h1>
+
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error.message}</p>}
+      {countries && (
+        <Countries
+          onCountryRemove={countryRemove}
+          countries={filteredCountries}
+        />
+      )}
     </div>
   );
 }
